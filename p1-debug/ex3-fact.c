e5fbc4325d6f7f5a29448fb70ba5d84947a7829b
GITTENKITTEN-123 <24f2100323@es.study.iitm.ac.in>
2025-01-04 09:19:57 +0000

    Solve 3 qns in part 1

--- p1-debug/ex3-fact.c
+++ p1-debug/ex3-fact.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-int n = 0;
-void fact(int n)
+int fact(int n)
 {
-    int i = 1;
-    for(; i <= n; i++)
-        n *= i;
+    int factorial = 1;
+    for(; n > 0; n--)
+    {
+        factorial *= n;
+    }
+    return factorial;
 }
 
 int main()
 {
-    fact(5);
+    int n = fact(5);
     printf("%d", n);
     return 0;
 }
